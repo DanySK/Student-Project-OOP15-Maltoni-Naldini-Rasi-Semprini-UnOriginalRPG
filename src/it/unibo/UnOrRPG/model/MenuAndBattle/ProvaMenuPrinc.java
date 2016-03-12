a02f5aa359bbc34a5217381365d03125da1655c9
luca semprini10 <luca.semprini10@studio.unibo.it>
2016-03-12 13:19:04 +0100

    Creata classe di prova per il menu principale

---
+++ src/it/unibo/UnOrRPG/model/MenuAndBattle/ProvaMenuPrinc.java
@@ -0,0 +1,25 @@
+package it.unibo.UnOrRPG.model.MenuAndBattle;
+
+import java.io.File;
+
+public class ProvaMenuPrinc  extends DummyMenu {
+	
+	public ProvaMenuPrinc() {}
+	
+	public static void newGame() {
+		
+		/*Avvia nuova partita*/
+		
+	}
+	
+	public static void caricaGame(File wheregameis) {
+		
+		/*Carica partita da file*/
+		
+	}
+	
+	public static void exit() {
+		
+		System.exit(0); //???
+	}
+}
