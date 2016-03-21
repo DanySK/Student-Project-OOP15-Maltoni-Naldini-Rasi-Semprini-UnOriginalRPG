170c8de201a13df995557fc6718779040b1dc551
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-03-21 13:52:08 +0100

    Added setLinkedMap, getState and setState methods

--- src/it/unibo/unori/model/maps/Cell.java
+++ src/it/unibo/unori/model/maps/Cell.java
@@ -2,6 +2,38 @@ package it.unibo.unori.model.maps;
 
 import java.io.Serializable;
 
+/**
+ * 
+ * A interface to define the basic method of a cell. A cell is the basic
+ * component of a map, a map has a matrix composed of various cells Every cell
+ * has a frame object inside , to represent the frame linked to the logical cell
+ * The logical cell has a position , a state ,which can be occupied or free,and
+ * various method to control it. Serializable interface is added in order to
+ * make possible the saving of a cell on file.
+ *
+ */
+
 public interface Cell extends Serializable {
+    /**
+     * Set a specific state on the cell.
+     * 
+     * @param state
+     *            State to set on the cell
+     */
+    public void setState(Object state);
+
+    /**
+     * Get the state of the cell.
+     * 
+     * @return the state of the Cell
+     */
+    public Object getState();
+
+    /**
+     * Set a specific Map in the cell as map to link.
+     * @param map
+     *          the map to link
+     */
+    public void setLinkedMap(Map map);
 
 }
