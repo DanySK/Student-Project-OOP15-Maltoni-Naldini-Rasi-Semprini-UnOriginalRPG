8db488c94625aaa776c6aec178161fc8a50919ed
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-03-22 17:10:06 +0100

    Switched package from maps to maps.cell

---
+++ src/it/unibo/unori/model/maps/cell/CellState.java
@@ -0,0 +1,16 @@
+package it.unibo.unori.model.maps.cell;
+
+/**
+ * 
+ * Describe the states which a cell can have.
+ *
+ */
+public enum CellState {
+    /**
+     * Describe the 3 state of a Cell.
+     * FREE : The cell is free and the party can walk on it
+     * BLOCKED : The cell is blocked and the party can't walk on it
+     * LINKED : The cell contains a map , when the party walks on it, a new map will be loaded
+     */
+    FREE, BLOCKED, LINKER;
+}
