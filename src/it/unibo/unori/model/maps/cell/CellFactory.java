f5ffa94a5e902ac6f003a97efb42e70651b419d9
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-03-22 17:34:14 +0100

    Created a factory for the cells

---
+++ src/it/unibo/unori/model/maps/cell/CellFactory.java
@@ -0,0 +1,11 @@
+package it.unibo.unori.model.maps.cell;
+
+/**
+ * 
+ * A Factory class to create the most common kind of Cells.
+ * It has only a few methods, in order to build automatically a few kind of cells
+ *
+ */
+public class CellFactory {
+
+}
