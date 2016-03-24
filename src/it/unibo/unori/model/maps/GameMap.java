084c21584f64e0df9d8984bed07780aebf365092
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-03-24 15:34:48 +0100

    Added getCell and setDimension method

--- src/it/unibo/unori/model/maps/GameMap.java
+++ src/it/unibo/unori/model/maps/GameMap.java
@@ -2,6 +2,21 @@ package it.unibo.unori.model.maps;
 
 import java.io.Serializable;
 
+/**
+ * 
+ * Interface to contains the basic methods for a game-map.
+ * Serializable interface is added in order to save Map-Object on file
+ *
+ */
 public interface GameMap extends Serializable {
+
+    /**
+     * Set the dimension of the current map. 
+     * @param width
+     *              the width of map in terms of number of cells
+     * @param length
+     *               the lenght of map in terms of number of cells
+     */
+    public void setDimension(int width, int length);
 
 }
