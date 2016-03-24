1ebe44692935dbf589893017e97b4cec0c3888d8
Stefano Rasi <stefano.rasi@studio.unibo.it>
2016-03-25 00:28:46 +0100

    Changed indentation

--- src/it/unibo/unori/view/View.java
+++ src/it/unibo/unori/view/View.java
@@ -12,43 +12,43 @@ import it.unibo.unori.view.layers.*;
  * The View class displays and contains game layers, it behaves like a stack
  *
  */
-public final class View extends JFrame {	
-	private static final String title = "UnRPG";
-	public static final Dimension size = new Dimension(800, 600);
+public final class View extends JFrame {    
+    private static final String title = "UnRPG";
+    public static final Dimension size = new Dimension(800, 600);
 
-	private final JLayeredPane layeredPane;
-	private final Stack<GameLayer> layerStack = new Stack<>();
+    private final JLayeredPane layeredPane;
+    private final Stack<GameLayer> layerStack = new Stack<>();
 
-	/**
-	 * Creates an instance of View
-	 */
-	public View() {
-		super(title);
-		setSize(size.width, size.height);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		layeredPane = getLayeredPane();
-	    
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override public void run() {
-				setVisible(true);
-			}
-		});
-	}
+    /**
+     * Creates an instance of View
+     */
+    public View() {
+        super(title);
+        setSize(size.width, size.height);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        layeredPane = getLayeredPane();
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override public void run() {
+                setVisible(true);
+            }
+        });
+    }
 
-	/**
-	 * Removes the layer at the top of the stack
-	 * @throws EmptyStackException  if this stack is empty.
-	 */
-	public final void popLayer() {
-		layeredPane.remove(layerStack.pop());
-	}
+    /**
+     * Removes the layer at the top of the stack
+     * @throws EmptyStackException  if this stack is empty.
+     */
+    public final void popLayer() {
+        layeredPane.remove(layerStack.pop());
+    }
 
-	/**
-	 * Pushes a layer onto the top of the stack
-	 * @param gameLayer  the layer to be pushed onto the stack
-	 */
-	public final void pushLayer(GameLayer gameLayer) {
-		layeredPane.add(layerStack.push(gameLayer));
-	}
+    /**
+     * Pushes a layer onto the top of the stack
+     * @param gameLayer  the layer to be pushed onto the stack
+     */
+    public final void pushLayer(GameLayer gameLayer) {
+        layeredPane.add(layerStack.push(gameLayer));
+    }
 }
--- src/it/unibo/unori/view/layers/MainMenuLayer.java
+++ src/it/unibo/unori/view/layers/MainMenuLayer.java
@@ -16,37 +16,37 @@ import javax.sound.midi.Sequencer;
 
 public class MainMenuLayer extends GameLayer {
 
-	/**
-	 * Creates an instance of the MainMenu layer
-	 */
-	public MainMenuLayer() {
-		setPreferredSize(View.size);
-		setBounds(0, 0, View.size.width, View.size.height);
-	}
+    /**
+     * Creates an instance of the MainMenu layer
+     */
+    public MainMenuLayer() {
+        setPreferredSize(View.size);
+        setBounds(0, 0, View.size.width, View.size.height);
+    }
 
-	/**
-	 * Draw the content of the main menu
-	 */
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-	}
+    /**
+     * Draw the content of the main menu
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
 
-	public static void main(final String... args) throws MidiUnavailableException,
-	                                                     IOException,
-	                                                     InvalidMidiDataException,
-	                                                     InterruptedException {
-		MainMenuLayer layer = new MainMenuLayer();
+    public static void main(final String... args) throws MidiUnavailableException,
+                                                         IOException,
+                                                         InvalidMidiDataException,
+                                                         InterruptedException {
+        MainMenuLayer layer = new MainMenuLayer();
 
-		View view = new View();
-		view.pushLayer(layer);
+        View view = new View();
+        view.pushLayer(layer);
 
-		Sequencer sequencer = MidiSystem.getSequencer();
-		InputStream is = new BufferedInputStream(new FileInputStream(new File("res/9.mid")));
-		sequencer.setSequence(is);
-		sequencer.open();
-		
-		Thread.sleep(2000);
-		sequencer.start();
-	}
+        Sequencer sequencer = MidiSystem.getSequencer();
+        InputStream is = new BufferedInputStream(new FileInputStream(new File("res/9.mid")));
+        sequencer.setSequence(is);
+        sequencer.open();
+        
+        Thread.sleep(2000);
+        sequencer.start();
+    }
 }
