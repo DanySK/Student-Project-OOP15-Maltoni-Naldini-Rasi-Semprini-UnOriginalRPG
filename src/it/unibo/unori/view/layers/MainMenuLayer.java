fc5e1c5e98e48b8a4adfb0ddda255c2222cb98a8
Stefano Rasi <stefano.rasi@studio.unibo.it>
2016-03-25 00:07:45 +0100

    MIDI Test

--- src/it/unibo/unori/view/layers/MainMenuLayer.java
+++ src/it/unibo/unori/view/layers/MainMenuLayer.java
@@ -4,6 +4,16 @@ import java.awt.Graphics;
 
 import it.unibo.unori.view.*;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequencer;
+
 public class MainMenuLayer extends GameLayer {
 
 	/**
@@ -22,10 +32,18 @@ public class MainMenuLayer extends GameLayer {
 		super.paintComponent(g);
 	}
 
-	public static void main(final String... args) {
+	public static void main(final String... args) throws MidiUnavailableException,
+	                                                     IOException,
+	                                                     InvalidMidiDataException {
 		MainMenuLayer layer = new MainMenuLayer();
-		
+
 		View view = new View();
 		view.pushLayer(layer);
+
+		Sequencer sequencer = MidiSystem.getSequencer();
+		InputStream is = new BufferedInputStream(new FileInputStream(new File("res/9.mid")));
+		sequencer.setSequence(is);
+		sequencer.open();
+		sequencer.start();
 	}
 }
