8b007caafafce4bf8511bf00ab596b0d1fe6d9df
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-03-30 16:01:51 +0200

    Removed Linker status for cells

--- src/it/unibo/unori/model/maps/cell/CellState.java
+++ src/it/unibo/unori/model/maps/cell/CellState.java
@@ -10,8 +10,7 @@ public enum CellState {
      * Describe the 3 state of a Cell.
      * FREE : The cell is free and the party can walk on it
      * BLOCKED : The cell is blocked and the party can't walk on it
-     * LINKED : The cell contains a map , when the party walks on it, a new map will be loaded
      */
-    FREE, BLOCKED, LINKER;
+    FREE, BLOCKED;
 
 }
