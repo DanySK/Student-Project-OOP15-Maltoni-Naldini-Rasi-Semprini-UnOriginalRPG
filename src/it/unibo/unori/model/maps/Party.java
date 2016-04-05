cb1212d50674e9ce4fbfcfbdaf8581b32e667943
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-04-05 17:19:46 +0200

    Added CardinalPoints enumeration

--- src/it/unibo/unori/model/maps/Party.java
+++ src/it/unibo/unori/model/maps/Party.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
  */
 public interface Party extends Serializable {
     /**
-     * set the current position of the party on the map.
+     * Set the current position of the party on the map.
      * @param posX
      *              X coordinate of the cell the party is on
      * @param posY
@@ -21,10 +21,56 @@ public interface Party extends Serializable {
     public void setCurrentPosition(int posX, int posY) throws IllegalArgumentException;
 
     /**
-     * set the current map for the party to walk on.
+     * Set the current map for the party to walk on.
      * @param map
      *          a map to walk on
      */
     public void setCurrentMap(GameMap map);
+
+    /**
+     * Set the current frame of party.
+     * @param frame
+     *              the frame to set
+     */
+    public void setCurrentFrame(Object frame);
+
+    /**
+     * Enum to define the four cardinal points and their skidding 
+     * from the current cell.
+     *
+     */
+    public enum CardinalPoints {
+        /**
+         * Four values for the cardinal points.
+         */
+        NORTH(1, 0), SOUTH(-1, 0), EST(0, 1), WEST(0, -1);
+
+        /**
+         * Parameters to save the skidding for each of the cardinal points.
+         */
+        private final int posX;
+        private final int posY;
+
+        private CardinalPoints(final int posX, final int posY) {
+           this.posX = posX;
+           this.posY = posY;
+        }
+
+        /**
+         * get the skidding of the X coordinate.
+         * @return the value of the skidding
+         */
+        public int getXSkidding() {
+            return this.posX;
+        }
+
+        /**
+         * get the skidding of the Y coordinate.
+         * @return the value of the skidding
+         */
+        public int getYSkidding() {
+            return this.posY;
+        }
+    }
 
 }
