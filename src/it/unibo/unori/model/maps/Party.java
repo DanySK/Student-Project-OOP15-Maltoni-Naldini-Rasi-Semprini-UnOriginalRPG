3b6dcef72461092840e701f933a57a65c4dda97b
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-04-05 17:00:14 +0200

    Changed type from Class to Interface, added setPosition and setMap methods

--- src/it/unibo/unori/model/maps/Party.java
+++ src/it/unibo/unori/model/maps/Party.java
@@ -1,5 +1,27 @@
 package it.unibo.unori.model.maps;
 
-public class Party {
+/**
+ * Party is the central object of the all map-related classes.
+ * A object which implements party must be free to move on the game map, and interact
+ * with Object on the GameMap
+ *
+ */
+public interface Party {
+    /**
+     * set the current position of the party on the map.
+     * @param posX
+     *              X coordinate of the cell the party is on
+     * @param posY
+     *              Y coordinate of the cell the party is on
+     *@throws IllegalArgumentException if the cell specified is blocked or absent
+     */
+    public void setCurrentPosition(int posX, int posY) throws IllegalArgumentException;
+
+    /**
+     * set the current map for the party to walk on.
+     * @param map
+     *          a map to walk on
+     */
+    public void setCurrentMap(GameMap map);
 
 }
