b2af08e1c95266ad1ed7ca8d4103876b3bef5256
luca_semprini10 <luca_semprini10@localhost>
2016-04-06 10:49:24 +0200

    Created Interface Potion

---
+++ src/it/unibo/unori/model/items/Potion.java
@@ -0,0 +1,10 @@
+package it.unibo.unori.model.items;
+
+/**
+ * An Interface modeling a generic Potion.
+ * A Potion is an Item that can restore some Character's statistics.
+ */
+public interface Potion extends Item {
+    
+  //TODO standard methods.
+}
