1825672dcec40037458620aebb9b59cb74aac950
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-04-12 16:12:31 +0200

    Added two methods to create a standard room and a lower linked map.

--- src/it/unibo/unori/model/maps/GameMapFactory.java
+++ src/it/unibo/unori/model/maps/GameMapFactory.java
@@ -1,6 +1,10 @@
 package it.unibo.unori.model.maps;
 
+import java.util.stream.IntStream;
+
+import it.unibo.unori.model.maps.cell.Cell;
 import it.unibo.unori.model.maps.cell.CellFactory;
+import it.unibo.unori.model.maps.cell.MapCellImpl;
 
 /**
  * Factory to realize some standard Maps.
@@ -22,6 +26,24 @@ public class GameMapFactory {
         map.setRow(MAXSIZE, FACT.getBlockedCell());
         map.setColumn(0, FACT.getBlockedCell());
         map.setColumn(MAXSIZE, FACT.getBlockedCell());
+        return map;
+    }
+
+    /**
+     * Create a Map whose lower bound is linked to another Map.
+     * @return the linked Map.
+     */
+    public GameMap getSouthLinkedMap() {
+        final GameMap map = new GameMapImpl();
+        map.setRow(MAXSIZE, FACT.getBlockedCell());
+        map.setColumn(0, FACT.getBlockedCell());
+        map.setColumn(MAXSIZE, FACT.getBlockedCell());
+        IntStream.range(0, MAXSIZE).forEachOrdered(i -> {
+            final Position pos = new Position(0, i);
+            final Cell cell = new MapCellImpl(new Object(), LINKINGMAP,
+                                    new Position(MAXSIZE - 1, i));
+            map.setCell(pos, cell);
+        });
         return map;
     }
 
