fa457683c908c9c5250e7f56e21272e9eb78033b
luca_semprini10 <luca_semprini10@localhost>
2016-04-13 12:11:28 +0200

    updated style.

--- src/it/unibo/unori/model/character/DummyCharacter.java
+++ src/it/unibo/unori/model/character/DummyCharacter.java
@@ -8,95 +8,95 @@ import it.unibo.unori.model.menu.DummyMenu;
  *
  */
 public class DummyCharacter implements Character {
-/**
+    /**
      * 
      */
     private static final long serialVersionUID = -1306119386793011379L;
 
-/**
- * something.
- * @return something.
- */
-    public DummyMenu createMenu() {
-        return new DummyMenu();
+    /**
+     * something.
+     * @return something.
+     */
+        public DummyMenu createMenu() {
+            return new DummyMenu();
+        }
+    
+    @Override
+    public int getRemainingHP() {
+        // TODO Auto-generated method stub
+        return 0;
     }
-
-@Override
-public int getRemainingHP() {
-    // TODO Auto-generated method stub
-    return 0;
-}
-
-@Override
-public int getTotalHP() {
-    // TODO Auto-generated method stub
-    return 0;
-}
-
-@Override
-public int getAttack() {
-    // TODO Auto-generated method stub
-    return 0;
-}
-
-@Override
-public int getDefense() {
-    // TODO Auto-generated method stub
-    return 0;
-}
-
-@Override
-public int getMagicAtk() {
-    // TODO Auto-generated method stub
-    return 0;
-}
-
-@Override
-public int getMagicDef() {
-    // TODO Auto-generated method stub
-    return 0;
-}
-
-@Override
-public int getSpeed() {
-    // TODO Auto-generated method stub
-    return 0;
-}
-
-@Override
-public int getLevel() {
-    // TODO Auto-generated method stub
-    return 0;
-}
-
-@Override
-public int getExpTot() {
-    // TODO Auto-generated method stub
-    return 0;
-}
-
-@Override
-public void addExp(final int expAcquired) {
-    // TODO Auto-generated method stub
     
-}
-
-@Override
-public int getRemainingExp() {
-    // TODO Auto-generated method stub
-    return 0;
-}
-
-@Override
-public void setWeapon(Weapon w) {
-    // TODO Auto-generated method stub
+    @Override
+    public int getTotalHP() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
     
-}
-
-@Override
-public Weapon getWeapon() {
-    // TODO Auto-generated method stub
-    return null;
-}
+    @Override
+    public int getAttack() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public int getDefense() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public int getMagicAtk() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public int getMagicDef() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public int getSpeed() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public int getLevel() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public int getExpTot() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public void addExp(final int expAcquired) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    @Override
+    public int getRemainingExp() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public void setWeapon(Weapon w) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    @Override
+    public Weapon getWeapon() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
