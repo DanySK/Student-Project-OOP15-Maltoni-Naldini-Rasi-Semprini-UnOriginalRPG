a3f02397411dbc996bb1f743779a0921cad8f1e1
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-04-13 17:09:43 +0200

    Completed Test for the Singleton Strategy.

--- src/it/unibo/unori/model/maps/PartyTest.java
+++ src/it/unibo/unori/model/maps/PartyTest.java
@@ -1,5 +1,8 @@
 package it.unibo.unori.model.maps;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 /**
@@ -12,13 +15,20 @@ public class PartyTest {
 
     /**
      * Test for the singleton strategy.
+     * Check if two variables stare at the same object, changing map and position
      */
     @Test
     public void testSingletonStrategy() {
         final Party party = SingletonParty.getParty();
         final Party party2 = SingletonParty.getParty();
         party.setCurrentMap(mapFactory.getStdRoom());
-
+        assertEquals(party.getCurrentPosition(), party2.getCurrentPosition());
+        final Position pos = new Position(10, 10);
+        final GameMap map = new GameMapImpl(20, 20);
+        map.setInitialCellPosition(pos);
+        party2.setCurrentMap(map);
+        assertEquals(party2.getCurrentPosition(), map.getInitialCellPosition());
+        assertTrue(party2.getCurrentPosition().equals(party.getCurrentPosition()));
     }
 
 }
