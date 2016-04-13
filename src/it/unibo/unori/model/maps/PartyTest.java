2628230ab3666d2be5e3ca2b404ece990a724d8e
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-04-13 16:54:09 +0200

    Added test foe the Singleton Strategy

--- src/it/unibo/unori/model/maps/PartyTest.java
+++ src/it/unibo/unori/model/maps/PartyTest.java
@@ -1,5 +1,24 @@
 package it.unibo.unori.model.maps;
 
+import org.junit.Test;
+
+/**
+ * Test Class for the Party object.
+ *
+ */
 public class PartyTest {
+
+    private final GameMapFactory mapFactory = new GameMapFactory();
+
+    /**
+     * Test for the singleton strategy.
+     */
+    @Test
+    public void testSingletonStrategy() {
+        final Party party = SingletonParty.getParty();
+        final Party party2 = SingletonParty.getParty();
+        party.setCurrentMap(mapFactory.getStdRoom());
+
+    }
 
 }
