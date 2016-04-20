b95e6f7dba0041a7643d64cd3253c241c333ed30
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-04-20 17:19:06 +0200

    Added constructor to save a caugth exeception.

--- src/it/unibo/unori/model/maps/exceptions/BlockedPathException.java
+++ src/it/unibo/unori/model/maps/exceptions/BlockedPathException.java
@@ -12,6 +12,22 @@ public class BlockedPathException extends Exception {
     private static final long serialVersionUID = 7282142092547170919L;
 
     /**
+     * Empty Constructor.
+     */
+    public BlockedPathException() {
+        super();
+    }
+
+    /**
+     * Constructor to catch and save another exception.
+     * @param e
+     *          Exception to catch and save.
+     */
+    public BlockedPathException(final Exception e) {
+        super(e);
+    }
+
+    /**
      * Send a message when the Exception is thrown.
      * 
      * @return the message to show
