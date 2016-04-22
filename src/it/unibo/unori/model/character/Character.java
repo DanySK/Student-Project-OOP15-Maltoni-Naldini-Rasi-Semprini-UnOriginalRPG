ea8a1d3609765fbab00b1a679dfd3be5c29422c4
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-04-22 16:22:27 +0200

    Created interface Character

---
+++ src/it/unibo/unori/model/character/Character.java
@@ -0,0 +1,12 @@
+package it.unibo.unori.model.character;
+
+import java.io.Serializable;
+
+/**
+ * Interface to define methods for all the characters(party members and enemies).
+ * Serializable is added in order to make possible saving character objects on file.
+ *
+ */
+public interface Character extends Serializable {
+
+}
