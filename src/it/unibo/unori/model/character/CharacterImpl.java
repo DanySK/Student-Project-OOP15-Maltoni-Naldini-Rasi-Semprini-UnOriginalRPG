390e2added4f13dec53c7a805964a0d265223b68
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-04-22 17:51:41 +0200

    Created CharacterImpl class

---
+++ src/it/unibo/unori/model/character/CharacterImpl.java
@@ -0,0 +1,116 @@
+package it.unibo.unori.model.character;
+
+/**
+ * Class to design a generic Character.
+ *
+ */
+public class CharacterImpl implements Character {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -95447626445744515L;
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getRemainingHP() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getTotalHP() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void consumeMP(int mpToConsume) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void restoreDamage(int hpToRestore) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int getAttack() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getDefense() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getMagicAtk() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getMagicDef() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getSpeed() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getFireDefense() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getThunderDefense() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getIceDefense() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getFireAtk() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getThunderAttack() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getIceAttack() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
