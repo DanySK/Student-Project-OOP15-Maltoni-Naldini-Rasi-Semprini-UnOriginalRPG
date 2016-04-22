2053213c670f4c7f7554650b894a41c8d747edf7
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-04-22 17:24:10 +0200

    Created Foe interface

---
+++ src/it/unibo/unori/model/character/Foe.java
@@ -0,0 +1,14 @@
+package it.unibo.unori.model.character;
+
+/**
+ * Interface modeling a generic Foe of the game.
+ */
+public interface Foe extends Character {
+
+    /**
+     * Get the IA of the foe.
+     * @return the IA of the foe
+     */
+    int getIA();
+
+}
