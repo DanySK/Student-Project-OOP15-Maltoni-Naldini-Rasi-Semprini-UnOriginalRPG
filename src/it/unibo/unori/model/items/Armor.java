65f4289a292389654928d1735ec864d2d32960a1
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-04-25 18:11:52 +0200

    Added getter methods for elemntal and physical defences

--- src/it/unibo/unori/model/items/Armor.java
+++ src/it/unibo/unori/model/items/Armor.java
@@ -1,14 +1,39 @@
 package it.unibo.unori.model.items;
 
+import it.unibo.unori.model.character.Status;
+
 /**
  * An Interface modeling a generic Armor.
  */
 public interface Armor extends Item {
-    
+
     /**
-     * This method gives me the improvement that one of the Statistics gets
-     * when the Character is equipped with the Armor.
-     * @return the improvement to the statistic.
+     * Get the physical defense of the armor.
+     * @return physical defense of the armor
      */
-    int getImprovement();
+    int getPhysicalRes();
+
+    /**
+     * Get the fire defense of the armor.
+     * @return fire defense of the armor
+     */
+    int getFireDef();
+
+    /**
+     * Get the thunder defense of the armor.
+     * @return the thunder defense of the armor
+     */
+    int getThunderDefense();
+
+    /**
+     * Get the ice defense of the armor.
+     * @return the ice defense of the armor
+     */
+    int getIceDefense();
+
+    /**
+     * Get the immunity to a particular status, if present.
+     * @return the status which the armor is immune to
+     */
+    Status getImmunity();
 }
