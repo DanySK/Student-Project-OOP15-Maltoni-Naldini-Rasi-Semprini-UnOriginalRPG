368f025929d6289b695ffc41334df0177457a35f
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-05-09 15:39:17 +0200

    Created class to generate basic statistics

---
+++ src/it/unibo/unori/model/character/jobs/StatisticsFactory.java
@@ -0,0 +1,15 @@
+package it.unibo.unori.model.character.jobs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import it.unibo.unori.model.character.Statistics;
+
+/**
+  *Class to generate the basic statistics of jobs.
+  */
+public class StatisticsFactory {
+    
+    private Map<Jobs, Map<Statistics, Integer>> jobsStatsMap = new HashMap<>();
+
+}
