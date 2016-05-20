287b93bc07c547502bc639964ebdb3e1625d90ac
luca_semprini10 <luca_semprini10@localhost>
2016-05-20 11:13:38 +0200

    created the basic MagicAttack.

--- src/it/unibo/unori/model/battle/MagicAttackGenerator.java
+++ src/it/unibo/unori/model/battle/MagicAttackGenerator.java
@@ -5,4 +5,12 @@ package it.unibo.unori.model.battle;
  */
 public class MagicAttackGenerator {
     
+    /**
+     * Method that generates a basic MagicAttack.
+     * @return a basic MagicAttack.
+     */
+    public MagicAttackInterface getStandard() {
+        return new MagicAttack("Schiaffo", "Che schiaffo!", 
+                "Non c'è nulla di magico in uno schiaffo", 0, 0, 0, 1, 10, 1);
+    }
 }
