adf4958fba7b7d1b532aec21ce0fdb6c01cd859e
luca_semprini10 <luca_semprini10@localhost>
2016-05-30 13:04:51 +0200

    implemented Interface Foe.

---
+++ src/it/unibo/unori/model/character/FoeImpl.java
@@ -0,0 +1,31 @@
+package it.unibo.unori.model.character;
+
+import java.util.Map;
+
+/**
+ * Implementation of Interface Foe.
+ */
+public class FoeImpl extends CharacterImpl implements Foe {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -1168567801329410379L;
+    private final int ia;
+    
+    /**
+     * Standard constructor for a Foe.
+     * @param ia the IA of the Foe.
+     * @param name the name of the Foe.
+     * @param map the Statistics of the Foe.
+     */
+    public FoeImpl(final int ia, final String name, final Map<Statistics, Integer> map) {
+        super(name, map);
+        this.ia = ia;
+    }
+    @Override
+    public int getIA() {
+        return this.ia;
+    }
+
+}
