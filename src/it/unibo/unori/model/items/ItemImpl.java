5ea6ac14295c9139a6ba6c2fd1c8f5603d8d8ff3
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-07-23 15:22:25 +0200

    Redefined HashCode and equals methods

--- src/it/unibo/unori/model/items/ItemImpl.java
+++ src/it/unibo/unori/model/items/ItemImpl.java
@@ -1,7 +1,5 @@
 package it.unibo.unori.model.items;
 
-import java.io.Serializable;
-
 /**
  * Class to implement a key object.
  *
@@ -12,18 +10,56 @@ public class ItemImpl implements Item {
      * 
      */
     private static final long serialVersionUID = 9211034710548463840L;
-    public final String 
+    private final String name;
+    private final String desc;
+    private static final int PRIME = 31;
+
+    /**
+     * Constructor with two input string, one for the name and 
+     * the other for the description.
+     * @param name
+     *          name of the item
+     * @param desc
+     *          description of the item
+     */
+    public ItemImpl(final String name, final String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
 
     @Override
     public String getName() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.name;
     }
 
     @Override
     public String getDescription() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.desc;
     }
+
+    @Override
+    public int hashCode() {
+
+        int result = 1;
+        result = PRIME * result + ((desc == null) ? 0 : desc.hashCode());
+        result = PRIME * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (this.getClass().equals(o.getClass())) {
+            final ItemImpl tmp = (ItemImpl) o;
+            return this.name.equals(tmp.getName()) 
+                    && this.desc.equals(tmp.getDescription());
+        } else {
+            return false;
+        }
+    }
+
+
 
 }
