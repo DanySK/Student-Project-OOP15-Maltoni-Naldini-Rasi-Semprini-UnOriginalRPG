b8250f042b7f3e6f56a57fabcf05ff83f4ea85a9
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-07-23 14:29:48 +0200

    Created ChestCellImpl

---
+++ src/it/unibo/unori/model/maps/cell/ChestCellImpl.java
@@ -0,0 +1,46 @@
+package it.unibo.unori.model.maps.cell;
+
+import it.unibo.unori.model.items.Bag;
+import it.unibo.unori.model.items.Item;
+import it.unibo.unori.model.items.ObjectItem;
+import it.unibo.unori.model.maps.exceptions.NoObjectFoundException;
+
+/**
+ * Class which design a chest in the game, which has a object inside and it need
+ * a key to open.
+ *
+ */
+public class ChestCellImpl extends SimpleCellImpl {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -7296644040007976176L;
+    private final ObjectItem o;
+
+    /**
+     * Constructor for the cell.
+     * @param frame
+     *              the frame to set in the cell
+     * @param o
+     *              object to put in the cell
+     */
+    public ChestCellImpl(final Object frame, final ObjectItem o) {
+        super(frame, CellState.BLOCKED);
+        this.o = o;
+    }
+
+    /**
+     * Method to open the chest and get the item inside.
+     * @param b
+     *          the bag of the party
+     * @return
+     *          the item in the chest
+     * @throws NoObjectFoundException if the chest is empty.
+     * 
+     */
+    public Item openChest(final Bag b) throws NoObjectFoundException {
+        return o;
+    }
+
+}
