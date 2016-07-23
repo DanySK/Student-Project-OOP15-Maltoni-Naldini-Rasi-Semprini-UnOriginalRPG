684cf79fd01bfe26f72ab5a1231ab802e3ea0c55
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-07-23 15:07:16 +0200

    Created ItemImpl class

---
+++ src/it/unibo/unori/model/items/ItemImpl.java
@@ -0,0 +1,29 @@
+package it.unibo.unori.model.items;
+
+import java.io.Serializable;
+
+/**
+ * Class to implement a key object.
+ *
+ */
+public class ItemImpl implements Item {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 9211034710548463840L;
+    public final String 
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
