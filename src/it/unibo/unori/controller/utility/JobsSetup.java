9047919cb960e24bfb490e69896988e36195d2a6
Niccol? Maltoni <Niccol? Maltoni@localhost>
2016-07-25 16:50:36 +0200

    Added Javadoc to JobsSetup Now I start to implement methods

--- src/it/unibo/unori/controller/utility/JobsSetup.java
+++ src/it/unibo/unori/controller/utility/JobsSetup.java
@@ -6,34 +6,83 @@ import it.unibo.unori.model.character.Statistics;
 import it.unibo.unori.model.items.Armor;
 import it.unibo.unori.model.items.Armor.ArmorPieces;
 import it.unibo.unori.model.items.Weapon;
-
-public class JobsSetup {
+/**
+ *  Utility class which provides default parameters for the jobs loading them from file.
+ */
+public final class JobsSetup {
+    /**
+     * Path to the JSON file of the warrior's default statistics and armors/weapons.
+     */
     public static final String WARRIOR = "Warrior.js";
+    /**
+     * Path to the JSON file of the paladin's default statistics and armors/weapons.
+     */
     public static final String PALADIN = "Paladin.js";
+    /**
+     * Path to the JSON file of the mage's default statistics and armors/weapons.
+     */
     public static final String MAGE = "Mage.js";
+    /**
+     * Path to the JSON file of the ranger's default statistics and armors/weapons.
+     */
     public static final String RANGER = "Ranger.js";
+    /**
+     * Path to the JSON file of the cook's default statistics and armors/weapons.
+     */
     public static final String COOK = "Cook.js";
+    /**
+     * Path to the JSON file of the clown's default statistics and armors/weapons.
+     */
     public static final String CLOWN = "Clown.js";
 
     private JobsSetup() {
         // Empty constructor
     }
 
+    /**
+     * Loads the default statistics of the job from JSON file. Suggeted to pass constants provided by this class.
+     * 
+     * @param path
+     *            the path to the file of the desired job.
+     * @return a map containing the values
+     */
     public static Map<Statistics, Integer> getDefaultStats(final String path) {
         // TODO
         return null;
     }
-    
+
+    /**
+     * Loads the default statistics increments of the job from JSON file. Suggeted to pass constants provided by this
+     * class.
+     * 
+     * @param path
+     *            the path to the file of the desired job.
+     * @return a map containing the values
+     */
     public static Map<Statistics, Integer> getDefaultIncrements(final String path) {
         // TODO
         return null;
     }
-    
+
+    /**
+     * Loads the default armor of the job from JSON file. Suggeted to pass constants provided by this class.
+     * 
+     * @param path
+     *            the path to the file of the desired job.
+     * @return a map containing the values
+     */
     public static Map<ArmorPieces, Armor> getDefaultArmor(final String path) {
         // TODO
         return null;
     }
-    
+
+    /**
+     * Loads the default weapon of the job from JSON file. Suggeted to pass constants provided by this class.
+     * 
+     * @param path
+     *            the path to the file of the desired job.
+     * @return a map containing the values
+     */
     public static Weapon getDefaultWeapon(final String path) {
         // TODO
         return null;
