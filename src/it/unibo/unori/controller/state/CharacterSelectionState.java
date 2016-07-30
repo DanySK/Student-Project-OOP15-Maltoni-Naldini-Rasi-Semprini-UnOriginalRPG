e55a16b4ef39bb2f7045bfeef7c13d54f3d684a1
Niccol? Maltoni <Niccol? Maltoni@localhost>
2016-07-31 00:55:50 +0200

    Added unimplemented CharacterSelectionState

---
+++ src/it/unibo/unori/controller/state/CharacterSelectionState.java
@@ -0,0 +1,32 @@
+package it.unibo.unori.controller.state;
+
+import javax.swing.JPanel;
+
+import it.unibo.unori.view.layers.CharacterSelectionLayer;
+
+public class CharacterSelectionState extends AbstractGameState {
+
+    public CharacterSelectionState() {
+        super(new CharacterSelectionLayer());
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void update(double elapsedTime) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onEnter() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onExit() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
