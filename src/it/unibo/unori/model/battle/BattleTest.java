8b7d06832604b7c7e8cafa3eac5568fb165459f3
luca_semprini10 <luca_semprini10@localhost>
2016-08-04 16:52:09 +0200

    modified BattleTest.

--- src/it/unibo/unori/model/battle/BattleTest.java
+++ src/it/unibo/unori/model/battle/BattleTest.java
@@ -1,6 +1,8 @@
 package it.unibo.unori.model.battle;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
