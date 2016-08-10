16d5021e93a2453374b530b8d3a56743936a5944
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-08-10 21:35:52 +0200

    Created finalFloorbuilder and secondFloorBuilder

--- src/it/unibo/unori/model/maps/DungeonBuilder.java
+++ src/it/unibo/unori/model/maps/DungeonBuilder.java
@@ -6,8 +6,10 @@ import java.util.List;
 import it.unibo.unori.model.items.Armor.ArmorPieces;
 import it.unibo.unori.model.items.ArmorFactory;
 import it.unibo.unori.model.items.Item;
+import it.unibo.unori.model.items.ItemImpl;
 import it.unibo.unori.model.items.PotionFactory;
 import it.unibo.unori.model.items.WeaponFactory;
+import it.unibo.unori.model.maps.cell.ChestCellImpl;
 import it.unibo.unori.model.maps.cell.MapCellImpl;
 import it.unibo.unori.model.maps.cell.ObjectCellImpl;
 
@@ -52,6 +54,10 @@ public class DungeonBuilder {
     private void storeItem(final Position pos, final GameMap map, final Item i) {
         map.setCell(pos, new ObjectCellImpl("", i));
     }
+    
+    private void storeChest(final Position pos, final GameMap map, final Item i) {
+        map.setCell(pos, new ChestCellImpl("", i));
+    }
 
     private void finalFloorBuilder() {
         for (int i = 0; i < 2; i++) {
@@ -60,6 +66,35 @@ public class DungeonBuilder {
         this.storeItem(POS1, fList.get(0), PACT.getGigaPozione());
         this.storeItem(POS2, fList.get(0), PACT.getTrapiantoMana());
         this.northLink(fList.get(0), fList.get(1));
+    }
+    
+    private void secondFloorBuilder() {
+        for (int i = 0; i < 18; i++) {
+            sList.add(FACT.getSizeableMap(8, 12));
+        }
+        this.northLink(sList.get(1), sList.get(0));
+        this.northLink(sList.get(2), sList.get(1));
+        this.westLink(sList.get(4), sList.get(1));
+        this.westLink(sList.get(3), sList.get(2));
+        this.storeItem(POS1, sList.get(4), PACT.getGranPozione());
+        this.westLink(sList.get(6), sList.get(3));
+        this.northLink(sList.get(7), sList.get(6));
+        this.northLink(sList.get(8), sList.get(7));
+        this.westLink(sList.get(8), sList.get(9));
+        this.storeChest(POS2, sList.get(9), WeaponFactory.getSpadaMistica());
+        this.northLink(sList.get(6), sList.get(5));
+        this.northLink(sList.get(5), sList.get(10));
+        this.storeItem(POS1, sList.get(10), AACT.getSilverEquip().get(ArmorPieces.SHIELD));
+        this.northLink(sList.get(10), sList.get(11));
+        this.westLink(sList.get(12), sList.get(11));
+        this.storeItem(POS1, sList.get(12), ItemImpl.KEY);
+        this.westLink(sList.get(13), sList.get(5));
+        this.westLink(sList.get(15), sList.get(13));
+        this.storeItem(POS1, sList.get(15), PACT.getGranPozione());
+        this.northLink(sList.get(14), sList.get(13));
+        this.westLink(sList.get(16), sList.get(14));
+        this.northLink(sList.get(17), sList.get(16));
+        
     }
 
     private void firstFloorBuilder() {
