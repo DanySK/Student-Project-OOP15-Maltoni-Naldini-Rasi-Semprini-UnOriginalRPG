ee7bf89a0aa58d285c2f0242f84e4d8c117e2419
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-08-10 11:54:23 +0200

    Added few methods to generate the map

--- src/it/unibo/unori/model/maps/GameMapFactory.java
+++ src/it/unibo/unori/model/maps/GameMapFactory.java
@@ -10,6 +10,7 @@ import it.unibo.unori.model.maps.cell.MapCellImpl;
  * Factory to realize some standard Maps.
  *
  */
+
 public class GameMapFactory {
     /**
      * Generic standard map.
@@ -46,6 +47,50 @@ public class GameMapFactory {
                                     new Position(MAXSIZE - 1, i));
             map.setCell(pos, cell);
         });
+        return map;
+    }
+
+    /**
+     * Create a map with the defined dimension.
+     * @param width
+     *          width of the map
+     * @param length
+     *          length of the map
+     * @return
+     *          a closed map sized as liked
+     */
+    public GameMap getSizeableMap(final int width, final int length) {
+        final GameMap map = new GameMapImpl(width + 1, length + 1);
+        final Cell cell = new CellFactory().getBlockedCell();
+        map.setRow(0, cell);
+        map.setRow(width, cell);
+        map.setColumn(0, cell);
+        map.setColumn(length, cell);
+        return map;
+        }
+    /**
+     * Create a room map.
+     * @return
+     *          a room map.
+     */
+    public GameMap createRoomMap() {
+        final GameMap map = this.getSizeableMap(5, 6);
+        map.setCell(new Position(2, 2), FACT.getBlockedCell());
+        return map;
+    }
+
+    /**
+     * Create the villageMap.
+     * @return villageMap.
+     */
+   public GameMap getVillageMap() {
+        final GameMap map = this.getSizeableMap(19, 17);
+        for (int i = 2; i < 6; i++) {
+            map.setCell(new Position(4, i), FACT.getBlockedCell());
+            map.setCell(new Position(5, i), FACT.getBlockedCell());
+            map.setCell(new Position(14, i), FACT.getBlockedCell());
+            map.setCell(new Position(15, i), FACT.getBlockedCell());
+        }
         return map;
     }
 
