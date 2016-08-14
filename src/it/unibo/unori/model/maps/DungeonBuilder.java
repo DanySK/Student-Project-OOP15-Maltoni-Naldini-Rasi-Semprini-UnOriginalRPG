97a53324694cd485eac595841dfddac5d180d98e
Federico Naldini <federico.naldini3@studio.unibo.it>
2016-08-14 16:53:09 +0200

    Setted default string in MapCellImpl path.

--- src/it/unibo/unori/model/maps/DungeonBuilder.java
+++ src/it/unibo/unori/model/maps/DungeonBuilder.java
@@ -37,12 +37,12 @@ public class DungeonBuilder {
      *          the upper map
      */
     private void northLink(final GameMap lowerMap, final GameMap upperMap) {
-       MapCellImpl c1 = new MapCellImpl("", upperMap, new Position(8, 6));
-       MapCellImpl c2 = new MapCellImpl("", upperMap, new Position(8, 7));
+       MapCellImpl c1 = new MapCellImpl(upperMap, new Position(8, 6));
+       MapCellImpl c2 = new MapCellImpl(upperMap, new Position(8, 7));
        lowerMap.setCell(new Position(0, 6), c1);
        lowerMap.setCell(new Position(0, 7), c2);
-       c1 = new MapCellImpl("", lowerMap, new Position(1, 6));
-       c2 = new MapCellImpl("", lowerMap, new Position(1, 7));
+       c1 = new MapCellImpl(lowerMap, new Position(1, 6));
+       c2 = new MapCellImpl(lowerMap, new Position(1, 7));
        upperMap.setCell(new Position(9, 6), c1);
        upperMap.setCell(new Position(9, 7), c2);
     }
@@ -55,12 +55,12 @@ public class DungeonBuilder {
      *          the east map
      */
     private void westLink(final GameMap westMap, final GameMap estMap) {
-        MapCellImpl c1 = new MapCellImpl("", estMap, new Position(4, 1));
-        MapCellImpl c2 = new MapCellImpl("", estMap, new Position(5, 1));
+        MapCellImpl c1 = new MapCellImpl(estMap, new Position(4, 1));
+        MapCellImpl c2 = new MapCellImpl(estMap, new Position(5, 1));
         westMap.setCell(new Position(4, 13), c1);
         westMap.setCell(new Position(5, 13), c2);
-        c1 = new MapCellImpl("", westMap, new Position(4, 12));
-        c2 = new MapCellImpl("", westMap, new Position(5, 12));
+        c1 = new MapCellImpl(westMap, new Position(4, 12));
+        c2 = new MapCellImpl(westMap, new Position(5, 12));
         estMap.setCell(new Position(4, 0), c1);
         estMap.setCell(new Position(5, 0), c2);
     }
@@ -73,8 +73,8 @@ public class DungeonBuilder {
      *          map to link
      */
     public void linkMap(final GameMap map1, final GameMap map2) {
-        final MapCellImpl c1 = new MapCellImpl("", map1, new Position(6, 5));
-        final MapCellImpl c2 = new MapCellImpl("", map2, new Position(6, 5));
+        final MapCellImpl c1 = new MapCellImpl(map1, new Position(6, 5));
+        final MapCellImpl c2 = new MapCellImpl(map2, new Position(6, 5));
         map2.setCell(new Position(5, 5), c1);
         map1.setCell(new Position(5, 5), c2);
     }
