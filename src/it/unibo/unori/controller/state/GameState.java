74630c4eaebe1d384bfdb314b6761e5191c54ca9
Niccol? Maltoni <niccolo.maltoni@studio.unibo.it>
2016-08-14 16:11:05 +0200

    Fixed and cleaned all the states

--- src/it/unibo/unori/controller/state/GameState.java
+++ src/it/unibo/unori/controller/state/GameState.java
@@ -1,34 +1,17 @@
 package it.unibo.unori.controller.state;
 
-import javax.swing.JPanel;
+import it.unibo.unori.view.layers.Layer;
 
 /**
- * This interface models a game state (eg. local map, world map, main menu, battle) in the state machine controller
+ * This interface models a game state (eg. local map, world map, main menu,
+ * battle) in the state machine controller
  */
 public interface GameState {
-
-    /**
-     * Updates data for each tick of the game. Called each frame for the currently active state.
-     * 
-     * @param elapsedTime
-     *            the time elapsed from start of the game state
-     */
-    void update(final double elapsedTime);
-
-    /**
-     * Called when entering in the state.
-     */
-    void onEnter();
-
-    /**
-     * Called when exiting from the state.
-     */
-    void onExit();
 
     /**
      * Return the graphic part of the GameState as a layer.
      * 
      * @return the graphic part of the state as a layer
      */
-    JPanel getLayer();
+    Layer getLayer();
 }
