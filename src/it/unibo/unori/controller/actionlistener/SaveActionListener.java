951200b5480aa91aa761df3e202d3376c2b8ae8c
Niccol? Maltoni <niccolo.maltoni@studio.unibo.it>
2016-08-15 16:47:30 +0200

    SaveActionListener completed.

--- src/it/unibo/unori/controller/actionlistener/SaveActionListener.java
+++ src/it/unibo/unori/controller/actionlistener/SaveActionListener.java
@@ -1,13 +1,22 @@
 package it.unibo.unori.controller.actionlistener;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
+import it.unibo.unori.controller.state.DialogState.ErrorSeverity;
+
+/**
+ * This ActionListener should be associated to save buttons to save the game.
+ */
 public class SaveActionListener extends AbstractUnoriActionListener {
 
     @Override
-    public void actionPerformed(ActionEvent event) {
-        // TODO Auto-generated method stub
-
+    public void actionPerformed(final ActionEvent event) {
+        try {
+            this.getController().saveGame();
+        } catch (IOException e) {
+            this.getController().showError(e.getMessage(), ErrorSeverity.SERIUOS);
+        }
     }
 
 }
