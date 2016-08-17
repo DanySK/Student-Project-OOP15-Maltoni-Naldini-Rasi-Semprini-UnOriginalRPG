3862bc9502e7ecd69c8b843947256196c92ec71a
Niccol? Maltoni <niccolo.maltoni@studio.unibo.it>
2016-08-18 01:03:14 +0200

    Now Map updates when you pick up an item

--- src/it/unibo/unori/controller/action/InteractAction.java
+++ src/it/unibo/unori/controller/action/InteractAction.java
@@ -1,5 +1,6 @@
 package it.unibo.unori.controller.action;
 
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import it.unibo.unori.controller.exceptions.NotValidStateException;
 import it.unibo.unori.controller.state.DialogState.ErrorSeverity;
 import it.unibo.unori.controller.state.MapState;
 import it.unibo.unori.model.menu.DialogueInterface;
+import it.unibo.unori.view.exceptions.SpriteNotFoundException;
 import it.unibo.unori.view.layers.MapLayer;
 
 /**
@@ -39,20 +41,33 @@ public class InteractAction extends AbstractAction {
     public void actionPerformed(final ActionEvent event) {
         if (MapState.class.isInstance(this.controller.getCurrentState())) {
             final MapState currentState = (MapState) this.controller.getCurrentState();
+            final MapLayer currentLayer = (MapLayer) currentState.getLayer(); 
             // If another dialogue is open, it tries to  next line ...
             if (this.currentDialogue.isPresent()) {
                 // ... if it can't, it closes the dialogue ...
                 if (this.currentDialogue.get().isOver()) {
-                    ((MapLayer) currentState.getLayer()).hideDialogue();
+                    (currentLayer).hideDialogue();
                     this.currentDialogue = Optional.empty();
                 // ... else shows the next line
                 } else {
-                    ((MapLayer) currentState.getLayer()).showDialogue(this.currentDialogue.get().showNext());
+                    (currentLayer).showDialogue(this.currentDialogue.get().showNext());
                 }
             // If there is no previous dialogue open, it loads a new one and shows it
             } else {
                 this.currentDialogue = Optional.of(currentState.interact());
-                ((MapLayer) currentState.getLayer()).showDialogue(this.currentDialogue.get().showNext());
+                (currentLayer).showDialogue(this.currentDialogue.get().showNext());
+            }
+            
+            if (currentState.checkMapChanges()) {
+                try {
+                    currentLayer.changeMap(currentState.getMap().getFrames(),
+                            new Point(currentState.getCurrentPosition().getPosX(),
+                                    currentState.getCurrentPosition().getPosY()));
+                } catch (SpriteNotFoundException e) {
+                    System.out.println("Error changing map in MoveAction:");
+                    this.controller.showError(e.getMessage(), ErrorSeverity.SERIUOS);
+                    e.printStackTrace();
+                }
             }
         } else {
             System.out.println("Error current state is not MapState in InteractAction:");
