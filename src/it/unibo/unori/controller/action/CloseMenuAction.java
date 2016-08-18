c39a3a985b61c4d043fcf24001bc75329f82d1cd
Niccol? Maltoni <niccolo.maltoni@studio.unibo.it>
2016-08-18 14:29:45 +0200

    Created CloseMenuAction

---
+++ src/it/unibo/unori/controller/action/CloseMenuAction.java
@@ -0,0 +1,38 @@
+package it.unibo.unori.controller.action;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import it.unibo.unori.controller.Controller;
+import it.unibo.unori.controller.SingletonStateMachine;
+import it.unibo.unori.controller.exceptions.NotValidStateException;
+import it.unibo.unori.controller.state.DialogState.ErrorSeverity;
+
+public class CloseMenuAction extends AbstractAction {
+    /**
+     * Generated serial version UID.
+     */
+    private static final long serialVersionUID = -6379981391618059109L;
+    private final Controller controller;
+    
+    /**
+     * Default constructor.
+     */
+    public CloseMenuAction() {
+        super();
+        this.controller = SingletonStateMachine.getController();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        try {
+            this.controller.closeMenu();
+        } catch (NotValidStateException e) {
+            this.controller.showError(e.getMessage(), ErrorSeverity.SERIUOS);
+            System.out.println("Error in close menu action:");
+            e.printStackTrace();
+        }
+    }
+
+}
