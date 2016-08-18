c771b4b46e3ee127084c126f7aa6084d5ca8aace
Niccol? Maltoni <niccolo.maltoni@studio.unibo.it>
2016-08-19 01:44:58 +0200

    EscapeActionListener should be OK

--- src/it/unibo/unori/controller/actionlistener/EscapeActionListener.java
+++ src/it/unibo/unori/controller/actionlistener/EscapeActionListener.java
@@ -2,12 +2,25 @@ package it.unibo.unori.controller.actionlistener;
 
 import java.awt.event.ActionEvent;
 
+import it.unibo.unori.controller.exceptions.NotValidStateException;
+import it.unibo.unori.controller.state.BattleState;
+import it.unibo.unori.controller.state.DialogState.ErrorSeverity;
+import it.unibo.unori.model.battle.exceptions.CantEscapeException;
+
 public class EscapeActionListener extends AbstractUnoriActionListener {
 
     @Override
     public void actionPerformed(final ActionEvent event) {
-        // TODO Auto-generated method stub
-
+        if (BattleState.class.isInstance(this.getController().getCurrentState())) {
+            final BattleState currentState = (BattleState) this.getController().getCurrentState();
+            try {
+                currentState.getModel().runAway();
+            } catch (CantEscapeException e) {
+                this.getController().showError(new NotValidStateException().getMessage(), ErrorSeverity.MINOR);
+            }
+        } else {
+            this.getController().showError(new NotValidStateException().getMessage(), ErrorSeverity.SERIUOS);
+        }
     }
 
 }
