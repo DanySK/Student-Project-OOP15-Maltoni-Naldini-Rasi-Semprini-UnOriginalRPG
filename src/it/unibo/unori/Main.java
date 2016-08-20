aa7786c59efd764a927d5e2591b9d2d43f894651
Stefano Rasi <stefano.rasi@studio.unibo.it>
2016-08-20 15:09:29 +0200

    Small fixes

--- src/it/unibo/unori/Main.java
+++ src/it/unibo/unori/Main.java
@@ -4,13 +4,14 @@ import it.unibo.unori.controller.Controller;
 import it.unibo.unori.controller.SingletonStateMachine;
 
 /**
- * This is the main class, the one that is launched at start, and the one that instantiates and starts the controller.
+ * This is the main class, the one that is launched at start, and the one that
+ * instantiates and starts the controller.
  */
 public final class Main {
     private Main() {
         // Empty constructor
     }
-
+    
     /**
      * First method to start.
      * 
@@ -19,8 +20,8 @@ public final class Main {
      */
     public static void main(final String... args) {
         final Controller c = SingletonStateMachine.getController();
-
+        
         c.begin();
     }
-
+    
 }
